71968a7faaf02beaab54a170c0423012f10b5d12
GuiNoronhaS <guinoronha7@gmail.com>
2020-12-11 22:09:29 -0300

    Exercicio 1

---
+++ js/exercicio4.js
@@ -0,0 +1,29 @@
+//Exercicio 4 - 1
+let arrayNotas = [9, 9 ,6];
+
+function calcularMedia() {
+    let Media = 0;
+    let nElement = 0;
+    array.forEach(element => {
+        nElement++;
+        Media += element;
+    });
+    console.log("Media", (Media/nElement));
+}
+//Exercicio 4 - 2
+
+//Exercicio 4 - 3
+//Exercicio 4 - 4
+//Exercicio 4 - 5
+//Exercicio 4 - 6
+//Exercicio 4 - 7
+//Exercicio 4 - 8
+//Exercicio 4 - 9
+//Exercicio 4 - 10
+//Exercicio 4 - 11
+//Exercicio 4 - 12
+//Exercicio 4 - 13
+//Exercicio 4 - 14
+//Exercicio 4 - 15
+//Exercicio 4 - 16
+//Exercicio 4 - 17
